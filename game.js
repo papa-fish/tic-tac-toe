d3f8aab29839d033fa5ac07eb4b1a9536a49558f
Luke Moore <Lukechristophermoore@gmail.com>
2023-06-08 11:36:49 +1000

    working tic-tac-toe game, need to style the page

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// -- game board global variables ======================
+// -- game board global variables ===============
 const winningCombinations = [
     [0, 1, 2],
     [0, 3, 6],
@@ -19,13 +19,22 @@ let playerTwoChoices = [];
 const playerChoice = document.querySelectorAll('.board div');
 const h2Elem = document.querySelector('.player-turn h2');
 const playAgainBtn = document.querySelector('#play-again');
-const spanElem = document.querySelector('span');
+const spanElem = document.querySelector('p span');
+const resetScoreBtn = document.querySelector('#reset-score-btn');
+const playerOneScore = document.querySelector('#player-one-score');
+const playerTwoScore = document.querySelector('#player-two-score');
 
-// -- 
+
+// -- event listeners ===========================
 for (let choice of playerChoice) {
     choice.addEventListener('click', handlePlayerTurn);
 };
 
+playAgainBtn.addEventListener('click', handlePlayAgain);
+
+resetScoreBtn.addEventListener('click', handleResetScore);
+
+// -- functions =================================
 function processPlayerTurn(tokenPlacement) {
 
     turnTracker++;
@@ -38,7 +47,8 @@ function processPlayerTurn(tokenPlacement) {
         playerOneChoices.push(tokenPlacementValue);
         h2Elem.innerText = "Player Two's Turn";
         h2Elem.style.backgroundColor = "#FFAEBC";
-        checkWinningCombinations(playerOneChoices)
+        checkWinningCombinations(playerOneChoices);
+        tokenPlacement.removeEventListener('click', handlePlayerTurn);
 
     } else {
 
@@ -47,23 +57,24 @@ function processPlayerTurn(tokenPlacement) {
         playerTwoChoices.push(tokenPlacementValue);
         h2Elem.innerText = "Player One's Turn";
         h2Elem.style.backgroundColor = "#B4F8C8";
-        checkWinningCombinations(playerTwoChoices)
+        checkWinningCombinations(playerTwoChoices);
+        tokenPlacement.removeEventListener('click', handlePlayerTurn);
     }
 
     spanElem.innerText = turnTracker;
-    isPlayerOneTurn = !isPlayerOneTurn;
 };
 
-function handlePlayerTurn(evt) {
+function handlePlayerTurn(event) {
     
-    const tokenPlacement = evt.target
+    const tokenPlacement = event.target
     processPlayerTurn(tokenPlacement);
  
-    if (turnTracker === 9) {  
+    if (turnTracker === 9 && checkWinningCombinations(playerOneChoices) === false) {  
         h2Elem.innerText = "DRAW";
         playAgainBtn.style.display = "inline-block";
         return;
     }
+    isPlayerOneTurn = !isPlayerOneTurn;
 };
 
 function checkWinningCombinations(array) {
@@ -72,9 +83,44 @@ function checkWinningCombinations(array) {
 
         const [num1, num2, num3] = combination;
         if (array.includes(num1) && array.includes(num2) && array.includes(num3)) {
-            playAgainBtn.style.display = "inline-block";   
+            playAgainBtn.style.display = "inline-block";
+
+            if (isPlayerOneTurn === true) {
+                h2Elem.innerText = "Player One Wins!";
+                h2Elem.style.backgroundColor = "#B4F8C8";
+                playerOneScore.innerText++;
+            } else if (isPlayerOneTurn === false) {
+                h2Elem.innerText = "Player Two Wins!";
+                h2Elem.style.backgroundColor = "#FFAEBC";
+                playerTwoScore.innerText++;
+            }
             return true;
         }  
     }
     return false;
+};
+
+function handlePlayAgain(event) {
+
+    isPlayerOneTurn = true;
+    turnTracker = 0;
+    spanElem.innerText = turnTracker;
+    h2Elem.innerText = "Player One's Turn";
+    h2Elem.style.backgroundColor = "#B4F8C8";
+    playerOneChoices = [];
+    playerTwoChoices = [];
+
+
+    for (let choice of playerChoice) {
+        choice.innerText = "";
+        choice.addEventListener('click', handlePlayerTurn);
+        choice.classList.remove('x');
+        choice.classList.remove('o');
+    }
+    playAgainBtn.style.display = "none";
+};
+
+function handleResetScore(event) {
+    playerOneScore.innerText = "0";
+    playerTwoScore.innerText = "0";
 };
